b75c56ceedcf257e71acca8820b42dcab34254ad
mowi_cdutten <cdutten@gmail.com>
2017-09-03 06:14:16 -0300

    Ver 1.1

--- calculadora.js
+++ calculadora.js
@@ -2,9 +2,12 @@
 
 var args = process.argv.slice(2);
 
+
 var operation = args[1];
+var operation2 = args[3]
 var num1 = parseFloat(args[0]);
 var num2 = parseFloat(args[2]);
+var num3 = parseFloat(args[4])
 
 var result = '\n Introduce bien los parámetros. \n';
 
@@ -24,6 +27,24 @@ if (args.length == 3){
 			break;
 	}
 }
+
+if (args.length == 5){
+	switch (operation2){
+		case "mas":
+			result = 'Suma: '+parseFloat(num1 + num2 + num3);
+			break;
+		case "menos":
+			result = 'Resta: '+parseFloat(num1 - num2 - num3);
+			break;
+		case "por":
+			result = 'Multiplicacion: '+parseFloat(num1 * num2 * num3);
+			break;	
+		case "dividido":
+			result = 'Division: '+parseFloat(num1 / num2 / num3);
+			break;
+	}
+}
+
 
 console.log(result);
 
